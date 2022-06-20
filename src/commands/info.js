4e2a09d706fe4e92af222ccbe8070169208d2711
cars <cara.blasius@gmail.com>
2022-06-20 19:13:07 +0200

    miesmuschel

---
+++ src/commands/info.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("User Informationen auslesen")
+    .addSubcommand(subCommand => subCommand.setName("server").setDescription("Zeige Informationen über den Server an"))
+    .addSubcommand(subCommand => subCommand
+                        .setName("member")
+                        .setDescription("Zeige Informationen über einen Member an")
+                        .addUserOption(option => option.setName("member")
+                        .setDescription("Der Member").setRequired(true))), 
+    async execute(interaction) {
+        switch(interaction.options.getSubcommand()){
+            case "server": {
+                interaction.reply({embeds: [
+                    new MessageEmbed()
+                    .setTitle(`Information für den Server ${interaction.guild.name}`)
+                    .addFields([
+                        {
+                            name: "Channels",
+                            value: `${interaction.guild.channels.cache.size} Channels`
+                        },
+                        {
+                            name: "Erstellt",
+                            value: `<t: ${Math.round(interaction.guild.createdTimestamp/1000)}>`,
+                            inline: true
+                        }
+                    ])
+                ]})
+                break
+            }
+
+            case "member": {
+                const member = interaction.options.getMember("member")
+                interaction.reply({embeds: [
+                    new MessageEmbed()
+                    .setTitle(`Information über ${member.user.tag}`)
+                    .setThumbnail(member.user.avatarURL({dynamic: true}))
+                    .addFields([
+                        {
+                            name: "Account erstellt",
+                            value: `<t:${Math.round(member.user.createdTimestamp/1000)}>`,
+                        },
+                        {
+                            name: "Server beigetreten",
+                            value: `<t:${Math.round(member.joinedTimestamp/1000)}>`,
+                            inline: true
+                        }
+                    ])
+                ]})
+                break
+            }
+        }
+    }
+}
---
+++ src/commands/miesmuschel.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("miesmuschel")
+    .setDescription("use if you need some guidance in your life")
+    .addStringOption(option => option.setName("question").setDescription("ask a specific quetion, the miesmuschel should answer").setRequired(true)), 
+    async execute(interaction) {
+        var advices = [
+            "Du stinkst",
+            "Nein",
+            "Ja",
+            "Frag mich nochmal in 2 Stunden",
+            "Deine Mutter sagt ja",
+            "vielleicht",
+            "42",
+            "Juckt mich nicht",
+            "Seh ich so aus als ob mich das interessiert?",
+            "Sicher nicht",
+            "Das weiß ich nicht",
+            "Theoretisch schon",
+            "Eigentlich nicht",
+            "Garantiert nicht",
+            "Das ist nicht so",
+            "Ich würde ja sagen",
+            "Das ist richtig",
+            "Frag mich nochmal",
+            "WEEEEEEEEEEEEEEEEEEEEEEEE",
+            "Nüsse sind gesund",
+            "Egal",
+            "Heute nicht",
+            "Wahrscheinlich",
+            "Unwahrscheinlich",
+            "Keine Ahnung",
+            "Eines Tages vielleicht",
+            "Keine Ahnung, im Zweifelsfall nimm Drogen",
+            "Bin beschäftigt"
+
+        ]
+        var rand = Math.floor(Math.random()*advices.length)
+        interaction.reply(`<:miesmuschel:988448308559216701> ${advices[rand]}`)
+    }
+}
