28a8a530662009c17009fd77a395f76f7c5dc020
cars <cara.blasius@gmail.com>
2022-06-23 18:10:38 +0200

    comment deploy-commands file

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -9,9 +9,6 @@ const path = require("path")
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v10")
 
-//Create a new rest client instance
-const restClient =  new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN)
-
 //Assemble and submit global commands
 const commands = []
 const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js") && !path.basename(file).startsWith("_"))
@@ -21,7 +18,23 @@ commandFiles.forEach(commandFile => {
     commands.push(command.data.toJSON())
 })
 
-restClient.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID),
- {body: commands})
-.then(() => console.log("SUCCESS: registered commands"))
-.catch(console.error)
+//Create a new rest client instance
+const restClient =  new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN)
+
+// #region FUNCTIONS
+async function submitCommands(commands, guildname = "") {
+    try {
+        if (guildname) {
+            await restClient.put(Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, process.env[`GUILD_ID_${guildname}`]), 
+            { body: commands })
+            .then(() => console.log(`Successfully reloaded application (/) commands for guild "${guildname}"`))
+        }else{
+            await restClient.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), 
+            { body: commands })
+            .then(() => console.log(`Successfully reloaded global application (/) commands`))
+        }
+    }catch (error) {
+        console.error(error);
+    }
+}
+// #endregion
