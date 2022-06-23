bae6793cbab90e1f31e83ce6557f8a9803007d25
cars <cara.blasius@gmail.com>
2022-06-23 22:24:15 +0200

    add 44 cat pictures

--- src/commands/animals.js
+++ src/commands/animals.js
@@ -1,11 +1,15 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
+const { MessageAttachment, MessageEmbed } = require("discord.js")
 
 exports.data = new SlashCommandBuilder()
     .setName("imsad")
     .setDescription("sends you a cute image")
 
 exports.execute = async (interaction) => {
-        const embed = new MessageEmbed().setImage('https://3.bp.blogspot.com/-e9iaysd5X1E/UDJmovqjrYI/AAAAAAAAAaE/lQVAJcSeCtA/s1600/catpicturess+(4).jpg')
-        interaction.reply({ embeds: [embed] })
+        const number =  Math.floor(Math.random()*44)
+        const file = new MessageAttachment(`assets/cats/${number}.jpg`)
+        const embed = new MessageEmbed()
+            .setImage(`attachment://${number}.jpg`)
+
+        interaction.reply({ embeds: [embed], files: [file] })
     }
