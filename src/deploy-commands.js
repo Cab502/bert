def6782b566d26a0f604a359fd05f2019f8b86b4
cars <cara.blasius@gmail.com>
2022-06-23 17:51:37 +0200

    comment deploy-commands file

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,18 +1,25 @@
+//Require the necessary encironment variables
 require("dotenv").config()
-//fs file system
+
+//Require the file system modules for dynamic command creation
 const fs = require("fs")
+const path = require("path")
+
+//Require the necessary discord.js classes
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v10")
-const commands = []
 
-const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js"))
+//Create a new rest client instance
+const restClient =  new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN)
+
+//Assemble and submit global commands
+const commands = []
+const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith(".js") && !path.basename(file).startsWith("_"))
 
 commandFiles.forEach(commandFile => {
     const command = require(`./commands/${commandFile}`)
     commands.push(command.data.toJSON())
 })
-
-const restClient =  new REST({version: "10"}).setToken(process.env.DISCORD_BOT_TOKEN)
 
 restClient.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID),
  {body: commands})
