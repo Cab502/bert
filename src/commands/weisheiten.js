079a5fba07e98951eff44a8b8526e93059d7d2cf
cars <cara.blasius@gmail.com>
2022-06-27 19:13:12 +0200

    fix

--- src/commands/weisheiten.js
+++ src/commands/weisheiten.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
 
 exports.data = new SlashCommandBuilder()
     .setName("weisheit")
     .setDescription("use if you need some wisdom")
 exports.execute = async (interaction) => {
         var wisdom = [
-            "Ist der Finger oben, wird man dich loben"
+            "Ist der Finger oben, wird man dich loben",
+            "test"
         ]
         var rand = Math.floor(Math.random()*wisdom.length)
 
