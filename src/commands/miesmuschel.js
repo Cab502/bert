a068eddc2ce8b1fea27f2a28b8218c6fce2338d0
cars <cara.blasius@gmail.com>
2022-07-06 19:35:57 +0200

    add advices.json

--- src/commands/miesmuschel.js
+++ src/commands/miesmuschel.js
@@ -1,49 +1,18 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
+const advices = require("../../assets/miesmuschel/advices.json")
 
 exports.data = new SlashCommandBuilder()
     .setName("miesmuschel")
     .setDescription("use if you need some guidance in your life")
     .addStringOption(option => option.setName("question").setDescription("ask a specific quetion, the miesmuschel should answer").setRequired(true)), 
 exports.execute = async (interaction) => {
-        var advices = [
-            "Du stinkst",
-            "Nein",
-            "Ja",
-            "Frag mich nochmal in 2 Stunden",
-            "Deine Mutter sagt ja",
-            "vielleicht",
-            "42",
-            "Na sicher",
-            "Auf jedenfall",
-            "Das bezweifel ich",
-            "Juckt mich nicht",
-            "Seh ich so aus als ob mich das interessiert?",
-            "Sicher nicht",
-            "Das weiß ich nicht",
-            "Theoretisch schon",
-            "Eigentlich nicht",
-            "Garantiert nicht",
-            "Das ist nicht so",
-            "Ich würde ja sagen",
-            "Das ist richtig",
-            "Frag mich nochmal",
-            "WEEEEEEEEEEEEEEEEEEEEEEEE",
-            "Nüsse sind gesund",
-            "Egal",
-            "Heute nicht",
-            "Wahrscheinlich",
-            "Unwahrscheinlich",
-            "Keine Ahnung",
-            "Eines Tages vielleicht",
-            "Keine Ahnung, im Zweifelsfall nimm Drogen",
-            "Bin beschäftigt"
-        ]
-        var rand = Math.floor(Math.random()*advices.length)
+        const advicesArr = advices.statements
+        var rand = Math.floor(Math.random()*advicesArr.length)
         const inputQuestion = interaction.options.getString('question');
 
         const embed = new MessageEmbed()
-            .addField(inputQuestion, `<:miesmuschel:988448308559216701> ${advices[rand]}`)
+            .addField(inputQuestion, `<:miesmuschel:988448308559216701> ${advicesArr[rand]}`)
 
         interaction.reply({ embeds: [embed] })
     }
